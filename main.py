5e38a58046df021992a07b2e9e6a4f77e158bd62
Ramazan Yildiz <ry.yildizramazan@gmail.com>
2024-05-03 21:35:44 +0300

    routes created

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("index.html")
+
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        return redirect(url_for("home"))
+    elif request.method == "GET":
+        return render_template("edit.html")
+
+
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        return redirect(url_for("home"))
+    elif request.method == "GET":
+        return render_template("add.html")
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
